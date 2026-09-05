45600c22c1b387bd60411bfabeb684d9043878ea
Prateek Kumar singh <prateekkumarsingh321@gmail.com>
2026-09-05 15:57:07 +0530

    Time: 1 ms (99.94%) | Memory: 77.1 MB (66.23%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+                //kadane's algorithm
+
+        int maxSum=Integer.MIN_VALUE;
+        int currSum=0;
+        for(int num: nums){
+            currSum +=num;
+            maxSum=Math.max(maxSum,currSum);
+            if(currSum <0) currSum=0;
+        }
+        return maxSum;
+    }
+}
