61acd95c0e933cf4d19905c190076f9db7bac7de
Prateek Kumar singh <prateekkumarsingh321@gmail.com>
2026-09-12 10:52:07 +0530

    Time: 1 ms (96.21%) | Memory: 47.7 MB (24.32%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,27 +1,45 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int row = matrix.length;
-        int col = matrix[0].length;
-        boolean checker = false;
-        for (int i = 0; i < row; i++) {
-            if (matrix[i][0] == 0) {
-                checker = true;
-            }
-            for (int j = 1; j < col; j++) {
-                if (matrix[i][j] == 0) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+
+        int col0=1;
+
+        // 1sr row and 1st colm as a marker
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]==0){
                     matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+                    if(j != 0){
+                        matrix[0][j] = 0;
+                    }else col0=0;
                 }
             }
         }
-        for (int i = row - 1; i >= 0; i--) {
-            for (int j = col - 1; j > 0; j--) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
+
+        // Set inner cells to zero using markers
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j] != 0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
                 }
             }
-            if (checker)
+        }
+
+        // now handling 1st row
+        if(matrix[0][0] ==0){
+            for(int j=0; j<m; j++){
+                matrix[0][j]=0;
+            }
+        }
+
+        //now handling 1st cols
+        if(col0==0){
+            for (int i = 0; i < n; i++) {
                 matrix[i][0] = 0;
+            }
         }
     }
 }
